ff81dfac1d34980e3513c52405e81e404d2f9f23
David Beniamine <david.beniamine@imag.fr>
2015-03-20 16:00:08 -0300

    Plot generator

---
+++ plotgen/plotter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -z "$1" ] || [ -z "$2" ]
+then
+    echo "Usage $0 exp-dir name [save]"
+    echo "dir:     Path to the experiment files"
+    echo "name:    Experiment name (common part of the experiment file names)"
+    echo "save:    Save the plot image, true or false. default: false"
+    exit 1
+fi
+save=F
+if [ ! -z "$3" ] && [ $3 == "true" ]
+then
+    save=T
+fi
+Rscript -e \
+    "require(knitr); path <- '$1'; name <- '$2'; save <-'$save'; knit2html(\"plot.rmd\", output='$1/$2-plots.html')"
+rm -rf $1/figure
+mv ./figure $1/
