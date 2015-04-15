a4067e8d5b9bbb36bde1e81f0d2225deaab36f92
David Beniamine <david.beniamine@imag.fr>
2015-04-15 12:21:26 -0300

    Working install package

--- plot.R
+++ plot.R
@@ -1,3 +1,19 @@
+# Copyright (C) 2015  Beniamine, David <David@Beniamine.net>
+# Author: Beniamine, David <David@Beniamine.net>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #Install dependencies, function from
 # http://stackoverflow.com/questions/24294433/installing-and-loading-packages-with-a-single-function
 # by OganM
@@ -7,7 +23,7 @@ insist  <-  function(name){
     name = as.character(name)
 
     if (!require(name, character.only = T, quiet=T)) {
-        install.packages(name)
+        install.packages(name, verbose=T, repos="http://cran.rstudio.com/")
         Sys.sleep(2)
         library(name, character.only = T)
     }
